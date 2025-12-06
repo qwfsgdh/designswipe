608212e18ef4d211f91d147d8cd1ca2e9f1a7a0e
qwfsgdh <arslangmail.shipulin@yandex.ru>
2025-12-06 19:32:32 +0900

    Fix remote image config esm

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,10 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
     remotePatterns: [
-      { protocol: "https", hostname: "**.unsplash.com" },
-      { protocol: "https", hostname: "images.unsplash.com" },
-      { protocol: "https", hostname: "plus.unsplash.com" },
-      { protocol: "https", hostname: "source.unsplash.com" },
-      { protocol: "https", hostname: "picsum.photos" },
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
     ],
   },
 };
