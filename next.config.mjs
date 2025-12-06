2ace898403e1bcdaf7ee4b23acd08cf04615f308
qwfsgdh <arslangmail.shipulin@yandex.ru>
2025-12-06 19:28:57 +0900

    Allow remote Unsplash/picsum images

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [{ protocol: "https", hostname: "images.unsplash.com" }],
+    remotePatterns: [
+      { protocol: "https", hostname: "**.unsplash.com" },
+      { protocol: "https", hostname: "images.unsplash.com" },
+      { protocol: "https", hostname: "plus.unsplash.com" },
+      { protocol: "https", hostname: "source.unsplash.com" },
+      { protocol: "https", hostname: "picsum.photos" },
+    ],
   },
 };
 
